e3bf1ae18a511bb1bfeea3a1de4ab784662fb471
oleksandr.pidhorodetskyi <s.pidgorodetsky@gmail.com>
2022-07-21 22:52:55 +0300

    del

--- Calender/js/constants.js
+++
@@ -1,8 +0,0 @@
-const backArrow = document.querySelector(".button__back");
-const forwardArrow = document.querySelector(".button__forward");
-const daysContainer = document.querySelector(".calendar__days");
-const title = document.querySelector(".header-calendar__title");
-
-export {
-    backArrow, forwardArrow, daysContainer, title
-};
